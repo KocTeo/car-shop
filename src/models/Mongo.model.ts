222ca3a9e28f92540d6b77555f66c3561cf0898e
Alexandre Teodoreto <alekteodoreto@hotmail.com>
2022-09-14 16:27:36 -0300

    feat: Create abstract class MongoModel

---
+++ src/models/Mongo.model.ts
@@ -0,0 +1,56 @@
+import { isValidObjectId, Model } from 'mongoose';
+import { IModel } from '../interfaces/IModel';
+
+const idInvalidMessage = 'ID inválido';
+
+abstract class MongoModel<T> implements IModel<T> {
+  protected _model: Model<T>;
+
+  constructor(model:Model<T>) {
+    this._model = model;
+  }
+
+  public async create(obj: T): Promise<T> {
+    const vehicle = await this._model.create({ ...obj }); 
+
+    return vehicle;
+  }
+
+  public async read(): Promise<T[]> {
+    const vehicles = await this._model.find();
+
+    return vehicles;
+  }
+
+  public async readOne(id: string): Promise<T | null> {
+    if (!isValidObjectId(id)) {
+      throw new Error(idInvalidMessage);
+    }
+
+    const vehicle = await this._model.findOne({ id });
+
+    return vehicle;
+  }
+
+  public async update(id: string, obj: T): Promise<T | null> {
+    if (!isValidObjectId(id)) {
+      throw new Error(idInvalidMessage);
+    }
+
+    const vehicle = await this._model.findOneAndUpdate({ id }, obj);
+
+    return vehicle;
+  }
+
+  public async delete(id: string): Promise<T | null> {
+    if (!isValidObjectId(id)) {
+      throw new Error(idInvalidMessage);
+    }
+
+    const vehicle = await this._model.findOneAndDelete({ id });
+
+    return vehicle;
+  }
+}
+
+export default MongoModel;
