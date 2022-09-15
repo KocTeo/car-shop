cb75f801f31b0abdc5d3b3886c184b74cb1a8f54
Alexandre Koch Teodoreto <91029472+KocTeo@users.noreply.github.com>
2022-09-15 17:55:24 -0300

    Update connection.ts

--- src/models/connection.ts
+++ src/models/connection.ts
@@ -1,9 +1,17 @@
 import mongoose from 'mongoose';
 import 'dotenv/config';
 
-const connectToDatabase = (
-  mongoDatabaseURI = process.env.MONGO_URI
-    || MONGO_DB_URL,
-) => mongoose.connect(mongoDatabaseURI);
+const mongoDatabaseURI = process.env.MONGO_URI
+
+function connectToDatabase() {
+  mongoose.connect(mongoDatabaseURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  
+  const db = mongoose.connection();
+  db.on('error', (error) => console.error(error));
+  db.once('open', () => console.log('Banco rodando'))
+}
 
 export default connectToDatabase;
