b53ad8281873a22617296a70445f774cb9b1b068
Alexandre Koch Teodoreto <91029472+KocTeo@users.noreply.github.com>
2022-09-16 15:58:43 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,6 @@ import connectToDatabase from './models/connection';
 const mongoURL = process.env.MONGO_URI as string;
 const port = process.env.PORT;
 
-connectToDatabase(mongoURL);
-
 app.listen(port, () => console.log('Node Rodando'));
+
+connectToDatabase(mongoURL);
